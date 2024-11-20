144398a6d30c8cdd432319d1b51d9e77c66541b6
MalteScharf <malte.scharf@hotmail.de>
2024-11-20 15:57:04 +0100

    Server Configuration

---
+++ Scripts/BaumyServer.py
@@ -0,0 +1,37 @@
+import socket
+import datetime
+import os
+
+# Get the directory of the current script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the file path
+file_path = os.path.join(script_dir, "Config.txt")
+
+# Open the file
+with open(file_path, "r") as file:
+    server_ip = file.read()
+
+port = 8002
+response = "Request received"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((server_ip, port))
+server.listen(0)
+print("Server now listening on port " + str(port))
+
+
+(client_socket, client_address) = server.accept()
+print("Accepted Connec.tion from" + str(client_address))
+
+while True:                                 # forever
+    request = client_socket.recv(1024)    # receive data from client
+    request = request.decode("utf-8")
+    print("Request from Client: " + str(request))
+    if not request: break                        # stop if client stopped
+    client_socket.send(response.encode("utf-8"))
+    print("Socket closed")
+
+
+client_socket.close()
+print("Socket closed")
--- Scripts/BrightestPixelDetector.py
+++ Scripts/BrightestPixelDetector.py
@@ -1,19 +1,19 @@
 import cv2 as cv2
 import numpy as np
+from cv2 import VideoCapture, waitKey, destroyWindow
 
 input_path = "/home/malte/Documents/Programming Projects/Baumy/Baumy/InputPictures/Input.jpeg"
-
-
 ## img laden
 img = cv2.imread(input_path)
 img = cv2.resize(img, (500 , 500))
 
-if img is None:
-    print("Image not found or path is incorrect")
-else:
-    print("Image loaded successfully")
+
 
 def get_brightest_pixels(img):
+    if img is None:
+        print("Image not found or path is incorrect")
+    else:
+        print("Image loaded successfully")
     gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     cv2.imshow('GrayImage', gray_image)
     cv2.waitKey(0)
@@ -22,10 +22,27 @@ def get_brightest_pixels(img):
 
     cv2.circle(img, maxLoc, radius=5, color=(0, 0, 255), thickness=-1)
 
-    return maxLoc
+    return maxLoc, maxVal
 
-maxLoc = get_brightest_pixels(img)
-print(maxLoc)
-cv2.imshow('Brightest Pixel', img)
+def takePicture():
+    cam = VideoCapture(0)
+    # Get the default frame width and height
+    frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    result, image = cam.read()
+
+    if result:
+        cv2.imshow("Test", image)
+        cv2.imwrite("Test", image)
+        waitKey(0)
+        destroyWindow("Test")
+    else:
+        print("No image detected. Please! try again")
+
+takePicture()
+## maxLoc = get_brightest_pixels(img)
+#print(maxLoc)
+#cv2.imshow('Brightest Pixel', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
