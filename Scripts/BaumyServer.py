695db88debd06377f1f8ef10b7c341687db94d4e
Malte Scharf <malte.scharf@hotmail.de>
2024-11-24 16:34:08 +0100

    Server Features added

--- Scripts/BaumyServer.py
+++ Scripts/BaumyServer.py
@@ -1,7 +1,8 @@
 import socket
 import datetime
 import os
-
+import sys
+from LEDControllerFunctions import LEDFunctions
 # Get the directory of the current script
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -20,18 +21,41 @@ server.bind((server_ip, port))
 server.listen(0)
 print("Server now listening on port " + str(port))
 
+ledcounter = 0
 
-(client_socket, client_address) = server.accept()
-print("Accepted Connecction from" + str(client_address))
 
-while True:                                 # forever
+while True: 
+    (client_socket, client_address) = server.accept()
+    print("Accepted Connecction from" + str(client_address))
+    
     request = client_socket.recv(1024)    # receive data from client
     request = request.decode("utf-8")
     print("Request from Client: " + str(request))
-    if not request: break                        # stop if client stopped
-    client_socket.send(response.encode("utf-8"))
-    print("Socket closed")
+    if not request: break    # stop if client stopped
+    elif request == 'closesrv':
+        client_socket.send(str("Server shutdown").encode("utf-8"))
+        client_socket.close()
+        print("Server closed")
+        sys.exit()
+    elif request == 'led':
+        led = LEDFunctions()
+        led.lightone(4, 255,255,255)
+        client_socket.send(str("Led on").encode("utf-8"))
 
+    elif request == 'next':
+        led = LEDFunctions()
+        led.lightone(ledcounter, 255,255,255)
+        client_socket.send(str("Led on").encode("utf-8"))
+        ledcounter +=1 ;
+    
+
+    elif request == 'led off':
+        led = LEDFunctions()
+        led.off()
+        client_socket.send(str("Led off").encode("utf-8"))
+
+
+    else: 
+        client_socket.send(response.encode("utf-8"))
 
 client_socket.close()
-print("Socket closed")
